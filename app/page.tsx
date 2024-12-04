cf3748739ab211bdf7e236b6d2cff516c5fcd617
bbek4210 <bhandaribibek731@gmail.com>
2024-12-04 08:04:57 +0545

    fix suspense error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
 import Home from "@/components/Home";
-import React from "react";
+import React, { Suspense } from "react";
 
 const page = () => {
   return (
     <div className="bg-gradient-to-br from-gray-900 via-purple-800 to-purple-500 h-screen">
       <div className="container mx-auto p-4 pt-6">
         {" "}
-        <Home />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Home />
+        </Suspense>
       </div>
     </div>
   );
